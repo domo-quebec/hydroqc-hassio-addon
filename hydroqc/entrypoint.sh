52c80396577ab5bcc7f6b374aff186a79fbcd480
Mathieu Dallaire <m@thieudallaire.net>
2022-02-28 20:10:31 +0000

    v0.2.3 - Fix issue where custom mqtt server is not working. - Hide custom config and MQTT server options - Update README - Remove unused files

--- hydroqc/entrypoint.sh
+++ hydroqc/entrypoint.sh
@@ -4,8 +4,8 @@ export MQTT_USERNAME=$(bashio::config 'mqtt.mqtt_username')
 export MQTT_PASSWORD=$(bashio::config 'mqtt.mqtt_password')
 export MQTT_HOST=$(bashio::config 'mqtt.mqtt_server')
 export MQTT_PORT=$(bashio::config 'mqtt.mqtt_port')
-export MQTT_DISCOVERY_DATA_TOPIC=$(bashio::config 'advanced.mqtt_discovery_data_topic')
-export MQTT_DATA_ROOT_TOPIC=$(bashio::config 'advanced.mqtt_data_root_topic')
+export MQTT_DISCOVERY_DATA_TOPIC=$(bashio::config 'mqtt.mqtt_discovery_data_topic')
+export MQTT_DATA_ROOT_TOPIC=$(bashio::config 'mqtt.mqtt_data_root_topic')
 export CONFIG_YAML=$(bashio::config 'advanced.hydroqc_config_file')
 export TZ=$(bashio::config 'advanced.timezone')
 export HQ2M_CONTRACTS_0_USERNAME=$(bashio::config 'hydro_quebec.hq_username')
@@ -16,11 +16,11 @@ export HQ2M_CONTRACTS_0_CONTRACT=$(bashio::config 'hydro_quebec.hq_contract')
 
 # Try Hassio MQTT Auto-Configuration
 
-if ! bashio::services.available "mqtt" && ! bashio::config.exists 'mqtt.server'; then
+if ! bashio::services.available "mqtt" && ! bashio::config.exists 'mqtt.mqtt_server'; then
     bashio::exit.nok "No internal MQTT service found and no MQTT server defined. Please install Mosquitto broker or specify your own."
 else
     bashio::log.info "MQTT available, fetching server detail ..."
-    if ! bashio::config.exists 'mqtt.server'; then
+    if ! bashio::config.exists 'mqtt.mqtt_server'; then
         bashio::log.info "MQTT server settings not configured, trying to auto-discovering ..."
         # MQTT_PREFIX="mqtt://"
         # if [ $(bashio::services mqtt "ssl") = true ]; then
@@ -31,7 +31,7 @@ else
         export MQTT_PORT=$(bashio::services mqtt "port")
         bashio::log.info "Configuring '$MQTT_HOST' mqtt server"
     fi
-    if ! bashio::config.exists 'mqtt.user'; then
+    if ! bashio::config.exists 'mqtt.mqtt_username'; then
         bashio::log.info "MQTT credentials not configured, trying to auto-discovering ..."
         export MQTT_USERNAME=$(bashio::services mqtt "username")
         export MQTT_PASSWORD=$(bashio::services mqtt "password")
