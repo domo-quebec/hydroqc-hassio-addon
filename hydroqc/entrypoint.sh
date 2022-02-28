735dc0c1aeeed0b70cd115fb6db3b5e0f0e9e47f
Mathieu Dallaire <m@thieudallaire.net>
2022-02-28 18:35:10 +0000

    Update hydroqc/entrypoint.sh Deleted hydroqc/hydroqc-config.yaml, hydroqc/requirements.txt.disabled

--- hydroqc/entrypoint.sh
+++ hydroqc/entrypoint.sh
@@ -16,11 +16,11 @@ export HQ2M_CONTRACTS_0_CONTRACT=$(bashio::config 'hydro_quebec.hq_contract')
 
 # Try Hassio MQTT Auto-Configuration
 
-if ! bashio::services.available "mqtt" && ! bashio::config.exists 'mqtt.server'; then
+if ! bashio::services.available "mqtt" && ! bashio::config.exists 'mqtt.mqtt_server'; then
     bashio::exit.nok "No internal MQTT service found and no MQTT server defined. Please install Mosquitto broker or specify your own."
 else
     bashio::log.info "MQTT available, fetching server detail ..."
-    if ! bashio::config.exists 'mqtt.server'; then
+    if ! bashio::config.exists 'mqtt.mqtt_server'; then
         bashio::log.info "MQTT server settings not configured, trying to auto-discovering ..."
         # MQTT_PREFIX="mqtt://"
         # if [ $(bashio::services mqtt "ssl") = true ]; then
@@ -31,7 +31,7 @@ else
         export MQTT_PORT=$(bashio::services mqtt "port")
         bashio::log.info "Configuring '$MQTT_HOST' mqtt server"
     fi
-    if ! bashio::config.exists 'mqtt.user'; then
+    if ! bashio::config.exists 'mqtt.mqtt_username'; then
         bashio::log.info "MQTT credentials not configured, trying to auto-discovering ..."
         export MQTT_USERNAME=$(bashio::services mqtt "username")
         export MQTT_PASSWORD=$(bashio::services mqtt "password")
