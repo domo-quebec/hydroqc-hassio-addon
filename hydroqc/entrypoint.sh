a3c4f1817b681a11c5c86619050109a83d7503ff
Julien Cassagne <julien.e.cassagne@gmail.com>
2022-08-31 11:50:50 -0400

    Handle HQ2M_SYNC_FREQUENCY env variable in conf

--- hydroqc/entrypoint.sh
+++ hydroqc/entrypoint.sh
@@ -8,6 +8,7 @@ export MQTT_DISCOVERY_DATA_TOPIC=$(bashio::config 'mqtt.mqtt_discovery_data_topi
 export MQTT_DATA_ROOT_TOPIC=$(bashio::config 'mqtt.mqtt_data_root_topic')
 export CONFIG_YAML=$(bashio::config 'advanced.hydroqc_config_file')
 export TZ=$(bashio::config 'advanced.timezone')
+export HQ2M_SYNC_FREQUENCY=$(bashio::config 'advanced.sync_frequency')
 export HQ2M_CONTRACTS_0_USERNAME=$(bashio::config 'hydro_quebec.hq_username')
 export HQ2M_CONTRACTS_0_PASSWORD=$(bashio::config 'hydro_quebec.hq_password')
 export HQ2M_CONTRACTS_0_CUSTOMER=$(bashio::config 'hydro_quebec.hq_customer')
